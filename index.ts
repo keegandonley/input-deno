fefe3c127d3736788673948d5468e7d79ff48d05
Keegan Donley <kdonley@pinpoint.com>
2020-05-26 18:03:11 -0500

    proper types

--- index.ts
+++ index.ts
@@ -2,24 +2,26 @@ export default class InputLoop {
 	buf = new Uint8Array(1024);
 	done = false;
 
-	read = async () => {
+	read = async (): Promise<string> => {
 		return new Promise(async (resolve, reject) => {
 			const n = await Deno.stdin.read(this.buf);
 
 			if (n) {
 				resolve(new TextDecoder().decode(this.buf.subarray(0, n)).replace('\n', ''));
+			} else {
+				reject();
 			}
 		});
 	}
 
-	choose = async (options: string[]) => {
+	choose = async (options: string[]): Promise<boolean[]> => {
 		options.forEach((option: string, index: number) => {
 			console.log(`${index}: ${option}`);
 		});
 
 		const result = await this.read();
 
-		return options.map((option: string, index: number) => {
+		return options.map((_option: string, index: number) => {
 			if (result === String(index)) {
 				return true;
 			}
@@ -27,7 +29,7 @@ export default class InputLoop {
 		});
 	}
 
-	question = (question: string) => {
+	question = (question: string): Promise<string> => {
 		console.log(question);
 
 		return this.read();
@@ -36,4 +38,5 @@ export default class InputLoop {
 	close = () => {
 		this.done = true;
 	}
+
 }
